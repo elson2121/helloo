a41875fe000d6b7ec29f52f0a220248df86d0e8f
elsen2121 <elsen21@gmail.com>
2025-04-11 08:59:05 -0900

    it is me

--- hel.cpp
+++ hel.cpp
@@ -1,7 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"fdtgdkhjcds"
-    return 0;
 
+
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9, 11, 13}; // Sorted array
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int target;
+    cout << "Enter the number to search: ";
+    cin >> target;
+
+    // Iterative Binary Search
+    int iterativeResult = binarySearchIterative(arr, size, target);
+    if (iterativeResult != -1)
+        cout << "Iterative: Element found at index " << iterativeResult << endl;
+    else
+        cout << "Iterative: Element not found" << endl;
+
+    // Recursive Binary Search
+    int recursiveResult = binarySearchRecursive(arr, 0, size - 1, target);
+    if (recursiveResult != -1)
+        cout << "Recursive: Element found at index " << recursiveResult << endl;
+    else
+        cout << "Recursive: Element not found" << endl;
+
+    return 0;
 }
